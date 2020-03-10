23a0302e2d3610ff6ebbe1c2bb3d2f5ad55ae2e4
sumardey5 <sumardey5@gmail.com>
2020-03-10 15:14:18 -0500

    Add serializeUser and deserializeUser

--- Workout-Tracker/config/passport.js
+++ Workout-Tracker/config/passport.js
@@ -30,3 +30,13 @@ passport.use(new GoogleStrategy ({
         });
     }
 ));
+
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+        done(err, user);
+    });
+});
