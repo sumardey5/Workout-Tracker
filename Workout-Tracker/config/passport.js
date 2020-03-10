bd88c35e245dfa13a1a07823d07572194c9bed69
sumardey5 <sumardey5@gmail.com>
2020-03-10 14:40:06 -0500

    Update PassportJS

--- Workout-Tracker/config/passport.js
+++ Workout-Tracker/config/passport.js
@@ -2,3 +2,14 @@ require('./config/database');
 require('./config/passport');
 
 const passport = require('passport');
+
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+
+passport.use(new GoogleStrategy ({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_SECRET,
+    callbackURL: process.env.GOOGLE_CALLBACK
+    },
+    function (acessToken, refreshToken, profile, cb) {
+    }
+));
