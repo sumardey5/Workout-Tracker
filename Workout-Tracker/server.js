b66588901fb4aabb5fd8355070644d065d2d7970
sumardey5 <sumardey5@gmail.com>
2020-03-10 14:08:06 -0500

    Add and configure middleware

--- Workout-Tracker/server.js
+++ Workout-Tracker/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const session = require('express-session');
 require('./config/database');
 
 
@@ -20,6 +21,11 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({
+  secret: 'Workout Tracker',
+  resave: false,
+  saveUninitialized: true
+}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
