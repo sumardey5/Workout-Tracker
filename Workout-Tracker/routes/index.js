59aab0dbaf99840a4743a00c33493b311a6af16e
sumardey5 <sumardey5@gmail.com>
2020-03-10 15:20:01 -0500

    Add login route for OAuth

--- Workout-Tracker/routes/index.js
+++ Workout-Tracker/routes/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET home page. */
+const passport = require('passport');
+
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/auth/google', passport.authenticate (
+  'google',
+  { scope: ['profile', 'email'] }
+));
 
 module.exports = router;
